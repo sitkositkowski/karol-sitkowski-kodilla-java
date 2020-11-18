724d93231b91a981d9eab9ee602ea6d2e032ce75
sitkositkowski <229982@student.pwr.edu.pl>
2020-11-18 22:14:58 +0100

    6.2 Added first unit test on class Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int a = 10;
+        int b = 13;
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(a, b);
+        int resultSubstract = calculator.substract(a, b);
+
+        System.out.println("Test - first unit test:");
+        if (resultAdd == a+b) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - second unit test:");
+        if (resultSubstract == a-b) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
