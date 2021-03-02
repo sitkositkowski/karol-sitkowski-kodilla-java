08c71d92c18cda903e21b31d880b737940974f15
sitkositkowski <sitkositkowski@wp.pl>
2021-03-03 00:47:49 +0100

    17.2 Added created TaskList entity

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.dao.TaskDao;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    private static final String LIST_NAME = "Test ListName";
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LIST_NAME,DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskLists = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals(1, readTaskLists.size());
+
+        //CleanUp
+        int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+
+    }
+}
