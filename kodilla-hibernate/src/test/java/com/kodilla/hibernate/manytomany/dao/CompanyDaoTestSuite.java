59bd3b06a2587140590593c5ddf3e52b8b3894fa
sitkositkowski <sitkositkowski@wp.pl>
2021-03-10 13:29:03 +0100

    17.4 Added named queries in Company package with separate tests

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -66,7 +66,53 @@ class CompanyDaoTestSuite {
     }
 
     @Test
-    void testNamedQueries() {
+    void testRetrieveCompaniesWithPrefix() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //When
+        List<Company> prefixCompany = companyDao.retrieveCompaniesWithPrefix("Dat");
+
+        //Then
+        assertEquals(1, prefixCompany.size());
+
+        //CleanUp
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    void testRetrieveEmployeesWithGivenLastname() {
         //Given
         Employee johnSmith = new Employee("John", "Smith");
         Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
@@ -97,11 +143,9 @@ class CompanyDaoTestSuite {
 
         //When
         List<Employee> lastnameEmployee = employeeDao.retrieveEmployeesWithGivenLastname("Smith");
-        List<Company> prefixCompany = companyDao.retrieveCompaniesWithPrefix("Dat");
 
         //Then
         assertEquals(1, lastnameEmployee.size());
-        assertEquals(1, prefixCompany.size());
 
         //CleanUp
         try {
